0846e99b354daadb93541e984eb7488255a4fb12
slavicf <jaroslavfrunt@gmail.com>
2017-11-21 17:32:55 +0200

    Проверил код по заданию 1

--- src/lessons/lesson2/task1.java
+++ src/lessons/lesson2/task1.java
@@ -1,4 +1,20 @@
 package lessons.lesson2;
 
+import java.util.Scanner;
+
 public class task1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите дробное A");
+        float a = scanner.nextFloat();
+
+        System.out.println("Введите дробное B");
+        float b = scanner.nextFloat();
+
+        System.out.println( a + " + " + b + " = " + (a + b) );
+
+    }
 }
