774fa31682f6077a98849a18fccf274c0bea3c86
slavicf <jaroslavfrunt@gmail.com>
2017-11-21 18:13:12 +0200

    Добавил класс с условием задания 4

---
+++ src/lessons/lesson2/task4.java
@@ -0,0 +1,17 @@
+package lessons.lesson2;
+
+public class task4 {
+
+    public static void main(String[] args) {
+
+//      Задание 4
+//      Дано 2 числа - A и B.
+//      Вывести в консоль:
+//      равны ли эти числа между собой (true / false)
+//      делиться ли A на B без остатка (true / false)
+//      A больше чем B (true / false)
+
+
+
+    }
+}
