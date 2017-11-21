68d768d52b3b687128632b64f96820d95263855f
slavicf <jaroslavfrunt@gmail.com>
2017-11-21 18:57:58 +0200

    Модуль 2. Задание 4. Добавил код

--- src/lessons/lesson2/task4.java
+++ src/lessons/lesson2/task4.java
@@ -1,5 +1,7 @@
 package lessons.lesson2;
 
+import java.util.Scanner;
+
 public class task4 {
 
     public static void main(String[] args) {
@@ -11,7 +13,23 @@ public class task4 {
 //      делиться ли A на B без остатка (true / false)
 //      A больше чем B (true / false)
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Введите число A: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите число B: ");
+        int b = scanner.nextInt();
+        System.out.println();
+
+        boolean equal = a == b;    // проверяем на равенство
+        System.out.println("A равно B? " + equal);
+
+        boolean remainder = (a % b) == 0; // проверяем на деление без остатка
+        System.out.println("Делится ли A на B без остатка? " + remainder);
+
+        boolean greater = a > b; // a > b?
+        System.out.println("A больше B? " + greater);
 
     }
 }
