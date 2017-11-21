530dc7ee12b766f6581cbfbf34d5469f08616102
slavicf <jaroslavfrunt@gmail.com>
2017-11-21 17:43:15 +0200

    Добавил комментарии с условием задания 1

--- src/lessons/lesson2/task2.java
+++ src/lessons/lesson2/task2.java
@@ -1,4 +1,20 @@
 package lessons.lesson2;
 
+import java.util.Scanner;
+
 public class task2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите ширину прямоугольника");
+        int a = scanner.nextInt();
+
+        System.out.println("Введите высоту прямоугольника");
+        int b = scanner.nextInt();
+
+        System.out.println( "Площадь прямоугольника составляет " + (a * b) );
+
+    }
 }
