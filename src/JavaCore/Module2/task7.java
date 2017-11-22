cf64c2994a1838ccb9c82c52a92205cb3e5d4d61
slavicf <jaroslavfrunt@gmail.com>
2017-11-22 19:34:53 +0200

    Модуль 2. Задание 7.

---
+++ src/JavaCore/Module2/task7.java
@@ -0,0 +1,28 @@
+package JavaCore.Module2;
+
+import java.util.Scanner;
+
+public class task7 {
+
+    public static void main(String[] args) {
+
+//        Задание 7 (дополнительное)
+//        Программа считывает с консоли цвет в формате RRGGBB и выводит текст Hello World в консоль этим цветом.
+
+//        РЕШЕНИЕ НЕ ПРЕДСТАВЛЯЕТСЯ ВОЗМОЖНЫМ, Т.К. ДОСТУПНЫ ТОЛЬКО 8 ЦВЕТОВ ТЕКСТА КОНСОЛИ
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите желаемый цвет из списка:");
+        System.out.println("0 - чёрный;\n1 - красный;\n2 - зелёный;\n3 - жёлтый;\n" +
+                "4 - синий;\n5 - пурпурный;\n6 - циан;\n7 - белый.");
+
+        int color = scanner.nextInt();
+
+        if (color >= 0 && color <=7){
+            System.out.print("\033[3" + color + "m");
+        }
+
+        System.out.println("Hello World");
+    }
+}
